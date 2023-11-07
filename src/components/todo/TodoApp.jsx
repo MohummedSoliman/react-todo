550fda757278a7cb5d85abb189e6e11ac3380fd8
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-07 21:45:11 +0200

    seperate header and footer components to seperate files

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Link, Route, Routes, useNavigate, useParams } from 'react-router-dom';
 import './TodoApp.css'
 import LogoutComponent from './LogoutComponent';
+import HeaderComponent from './HeaderComponent';
 
 export default function TodoApp(){
     return (
@@ -18,7 +19,6 @@ export default function TodoApp(){
                     <Route path='/*' element={< ErrorComponent />} />
 
                 </Routes>
-                <FooterComponent/>
             </BrowserRouter>
         </div>
     )
@@ -150,40 +150,5 @@ function ListTodosComponent(){
                </table>
            </div>
         </div>
-    )
-}
-
-function HeaderComponent() {
-    return (
-        <header className="border-bottom border-light border-5 mb-5 p-2">
-            <div className="container">
-            <div className="row">
-                <nav className="navbar navbar-expand-lg">
-                    <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.in28minutes.com">in28minutes</a>
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav">
-                            <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li>
-                            <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
-                        </ul>
-                    </div>
-                    <ul className="navbar-nav">
-                        <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
-                        <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
-                    </ul>
-                </nav>
-            </div>
-            </div>
-        </header>
-
-    )
-}
-
-function FooterComponent()  {
-    return (
-        <footer className="footer">
-            <div className="container">
-              Footer .
-            </div>
-        </footer>
     )
 }
