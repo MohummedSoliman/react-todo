178c21f0150ca5573a1a5798acdfe28194518762
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-09 11:01:20 +0200

    add update todo form to enable description and targetdate

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { useAuth } from "../security/AuthContext";
 import { retrieveTodoAPI } from "./api/RestTodoApiService"
+import { Formik, Form, Field } from "formik";
 
 export default function TodoComponent(){
 
@@ -28,12 +29,36 @@ export default function TodoComponent(){
             .catch( error => console.log(error));
     }
 
+    function onSubmit(values){
+        console.log(values)
+    }
+
     return (
         <div className="container">
             <h1>Enter Todo Details</h1>
-            <form>
-                Description: { description }
-            </form>
+            <div>
+                <Formik initialValues={ { description, targetDate} }
+                        enableReinitialize = { true }
+                        onSubmit = { onSubmit }>
+                {
+                (props) => {
+                    return <Form>
+                           <fieldset className="form-group">
+                               <label>Description</label>
+                               <Field type="text" name="description" className="form-control"/>
+                           </fieldset>
+                           <fieldset className="form-group">
+                               <label>Target Date</label>
+                               <Field type="date" name="targetDate" className="form-control"/>
+                           </fieldset>
+                           <div>
+                               <button className="btn btn-success m-5" type="submit">Save</button>
+                           </div>
+                        </Form>
+                    }
+                }
+                </Formik>
+            </div>
         </div>
     )
 }
